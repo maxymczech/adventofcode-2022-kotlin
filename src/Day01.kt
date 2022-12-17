35ba5a21e96867ae78a2fba0ac5570133e11cc00
Maksym Shcherban <peregrin@ukr.net>
2022-12-17 13:21:00 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,20 @@
+import java.io.File
+import java.io.BufferedReader
+
+fun readTextFile(name: String): String {
+  val bufferedReader: BufferedReader = File(name).bufferedReader()
+  var str = bufferedReader.use { it.readText() }
+  return str
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val str = readTextFile("day01.txt")
+    val arr = str.split("\r\n\r\n").map{ it.lines().map{ it.toInt() }.sum() }.toIntArray()
+    arr.sortDescending()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    // Part 1
+    println(arr[0])
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    // Part 2
+    println(arr[0] + arr[1] + arr[2])
 }
